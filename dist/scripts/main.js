a3c140cfecbb09bbcf002d201dc0ad0143ac669f
Norman Heck <Norman.Heck@gmail.com>
2017-01-24 19:45:48 +0100

    event changes

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -559,6 +559,22 @@ function initialize() {
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
+/*
+* Scrollspy Hashchange
+* Hash change on scroll with Bootstrap 3 Scrollspy
+* Author: Franco Moya - @iamravenous
+*/
+
+(function($) {
+  $(document).ready(function (){
+    'use strict';
+
+    $(window).on('activate.bs.scrollspy', function (e) {
+      history.replaceState({}, "", $("a[href^='#']", e.target).attr("href"));
+    });
+  });
+})(jQuery);
+
 /* ========================================================================
  * Bootstrap: scrollspy.js v3.3.7
  * http://getbootstrap.com/javascript/#scrollspy
