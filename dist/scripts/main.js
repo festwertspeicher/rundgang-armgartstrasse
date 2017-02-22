bc0c3c24ef709204176be41e7f867790a7553f89
Norman Heck <Norman.Heck@gmail.com>
2017-02-22 15:34:08 +0100

    slick-slider update

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1030,23 +1030,54 @@ var slick = (function () {
   "use strict";
 
   var pub = {}, // Stands for public, gets returned
-    slider, doNotHoverMobile, slickPrev, slickNext, slickTrack
+    $slider, $status, $customPrevArrow, $customNextArrow
 
   pub.init = function () {
-    slider = $(".slider");
+    $slider = $(".slider");
+    $status = $("#pagination");
+    $customPrevArrow = $(".slick-prev");
+    $customNextArrow = $(".slick-next");
 
-    slider.slick({
-      lazyLoad: 'ondemand',
+    $slider.on('init reInit afterChange', pub.updateCounter);
+    $slider.on('click', pub.nextSlide);
+    $customPrevArrow.on('click', pub.prevSlide);
+    $customNextArrow.on('click', pub.nextSlide);
+
+    $slider.slick({
+      slide: ".picture",
+      lazyLoad: "ondemand",
+      dots: false,
+      infinite: true,
+      fade: true,
+      speed: 250,
+      cssEase: "linear",
+      swipeToSlide: true,
+      adaptiveHeight: true,
+      prevArrow: false,
+      nextArrow: false
     });
-
   };
+
+  pub.updateCounter = function(event, slick, currentSlide, nextSlide) {
+    //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    $status.text("Bild " + i + "/" + slick.slideCount);
+  };
+
+  pub.nextSlide = function(event) {
+    $slider.slick('slickNext');
+  }
+
+  pub.prevSlide = function(event) {
+    $slider.slick('slickPrev');
+  }
 
   $(document).ready( function () {
       pub.init();
   });
 
   return pub;
-}());
+}(jQuery));
 
 
 /* ========================================================================
